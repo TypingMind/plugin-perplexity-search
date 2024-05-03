289570b4f9979daf0d05769f54152ebe5373ca33
VA <lehuuvietanh@gmail.com>
2024-05-03 17:23:56 +0700

    perplexity-search plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+This plugin allows the AI assistant to search for information from the internet using Perplexity.
+
+**🔑 Perplexity API Key needed**. Click the Settings tab and enter your API Key. Get your Perplexity API Key from [here](https://www.perplexity.ai/settings/api)
+
+Example usage:
+
+> What's the gold price?
+
+> How's the weather at HCMC at the moment?
---
+++ implementation.js
@@ -0,0 +1,38 @@
+function search_via_perplexity(params, userSettings) {
+  const keyword = params.keyword;
+  const model = userSettings.model || 'sonar-medium-online';
+  const systemMessage = userSettings.systemMessage || 'Be precise and concise.';
+  const key = userSettings.apiKey;
+
+  if (!key) {
+    throw new Error(
+      'Please set the Perplexity API Key in the plugin settings.'
+    );
+  }
+
+  return fetch('https://api.perplexity.ai/chat/completions', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+      authorization: 'Bearer ' + key,
+    },
+    body: JSON.stringify({
+      model: model,
+      messages: [
+        {
+          role: 'system',
+          content: systemMessage,
+        },
+        {
+          role: 'user',
+          content: keyword,
+        },
+      ],
+    }),
+  })
+    .then((r) => r.json())
+    .then((response) => {
+      return response.choices.map((c) => c.message.content).join(' ');
+    });
+}
